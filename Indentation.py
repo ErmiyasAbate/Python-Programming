e2eb4cc8b5172ef90c8d81b795c1af0eab6680a3
Ermiyas Abate <ermiyasabateg@gmail.com>
2021-12-10 00:02:14 +0300

    Python Variable added

---
+++ Indentation.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3, 4]
+
+for i in list1:
+    print(i)
+    if i == 4:
+        break
+print("End of the line")
+
+# Single line comment
+"""
+Multi line comment
+Multi line comment
+Multi line comment
+"""
---
+++ Python Variable/VaribaleName.py
@@ -0,0 +1,23 @@
+name = "Devansh"
+age = 20
+marks = 80.50
+
+print(type(name))
+print(type(age))
+print(type(marks))
+
+print(name)
+print(age)
+print(marks)
+
+name = "A"
+Name = "B"
+naMe = "C"
+NAME = "D"
+n_a_m_e = "E"
+_name = "F"
+name_ = "G"
+_name_ = "H"
+na56me = "I"
+
+print(name, Name, naMe, NAME, n_a_m_e, NAME, n_a_m_e, _name, name_, _name, na56me)
