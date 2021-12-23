5ac73fd129731f62247949af07bdafdeb08189ae
Ermiyas Abate <ermiyasabateg@gmail.com>
2021-12-23 13:33:06 +0300

    - Boolean Literals.py - Numeric Literals.py - String Literals.py

---
+++ Python Literals/Numeric Literals.py
@@ -0,0 +1,9 @@
+x = 100
+y = 100.5
+z = 5 + 3.13j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(z)
---
+++ Python Literals/String Literals.py
@@ -0,0 +1,12 @@
+a = "Hello"  # Double Quotes
+b = '12345'  # Single Quotes
+
+print(a + b)
+
+text1 = "Hello\nWorld"
+print(text1)
+
+text2 = """Hello,
+Python
+"""
+print(text2)
