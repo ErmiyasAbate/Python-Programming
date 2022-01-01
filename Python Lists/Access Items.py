2834a8fbfa03119a263f9f8f87e7df257a3d8e63
Ermiyas Abate <ermiyasabateg@gmail.com>
2022-01-01 19:36:06 +0300

    - Python String - Python list and List Access

---
+++ Python Lists/Access Items.py
@@ -0,0 +1,14 @@
+list1 = ["apple", "banana", "cherry"]
+print(list1[1])
+print(list1[-1])
+
+a = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(a[1:5])
+print(a[:5])
+print(a[2:])
+print(a[-4:-1])
+
+if "cherry" in a:
+    print("Yes, Cherry is in the list")
+else:
+    print("No, Cherry is not in the list")
---
+++ Python Lists/Python Lists.py
@@ -0,0 +1,15 @@
+a = ["apple", "banana", "cherry"]
+print(a)
+c = len(a)
+print(len(a))
+print(c)
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 2, 3, 4, 5, 6]
+list3 = [True, False, False]
+list4 = ["abc", 34, True, 40, "male"]
+print(list4)
+print(type(list3))
+
+list5 = list(("apple", "banana", "cherry"))
+print(list5)
