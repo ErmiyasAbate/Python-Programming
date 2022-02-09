705f2bb144dfa9ba37c41748c95317812f43badb
ErmiyasAbate <ermiyasabateg@gmail.com>
2022-02-10 00:54:10 +0300

    Python Scope

---
+++ Python Scope/Local Scope Two.py
@@ -0,0 +1,7 @@
+def my_function():
+    x = 300
+    def myinnerfunc():
+        print(x)
+    myinnerfunc()
+
+my_function()
---
+++ Python Scope/Local Scope.py
@@ -0,0 +1,5 @@
+def my_function():
+    x = 300 # Local Variable
+    print(x)
+
+my_function()
