60de0d87ce29cf161aa09462a9679585c9ff6702
ErmiyasAbate <ermiyasabateg@gmail.com>
2022-02-10 20:46:18 +0300

    Python Math

---
+++ Python Math/Built-in Math Functions.py
@@ -0,0 +1,4 @@
+print(min(5, 10, 25))
+print(max(5, 10, 25))
+print(abs(-7.25))
+print(pow(2,3))
---
+++ Python Math/Math Module.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.sqrt(81))
+print(math.pi)
+print(math.sin(0))
+print(math.ceil(1.4))
+print(math.floor(1.4))
